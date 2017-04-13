7d48cde76af11b9cd0afa3ea0d8ac864c481946c
Winston Chu <wpchu@andrew.cmu.edu>
2017-04-13 01:19:04 -0400

    login modal and flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8049,5 +8049,10 @@ Picker.extend( 'pickadate', DatePicker )
 }( jQuery ));
 
 $(document).ready(function() {
-    $('select').material_select();
-  });
+  $('select').material_select();
+});
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
