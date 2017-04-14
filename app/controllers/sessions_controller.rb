bd21778f1b597ebd097013ef01089a4a27e99b57
Winston Chu <wpchu@andrew.cmu.edu>
2017-04-13 21:44:20 -0400

    changed logout message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Goodbye, #{user.first_name}!"
+    redirect_to root_url, notice: "Logged out."
   end
 end
