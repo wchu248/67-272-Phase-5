05ecd95711255f06ac55b0ca9e25425f3cecd8a1
Winston Chu <wpchu@andrew.cmu.edu>
2017-04-13 22:00:49 -0400

    fixed user authenticate thing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    if user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Welcome, #{user.first_name}!"
     else
