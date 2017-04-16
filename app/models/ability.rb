6a05562aac282c88b651da79f7c833b52f416dab
Winston Chu <wpchu@andrew.cmu.edu>
2017-04-15 20:03:09 -0400

    started ability file, starting user controller

---
+++ app/models/ability.rb
@@ -0,0 +1,26 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # set user to new User if not logged in
+    user ||= User.new # i.e., a guest user
+
+    if user.role? :admin
+      # admins can do everything
+      can :manage, :all
+    elsif user.role? :manager
+      nil
+    elsif user.role? :shipper
+      nil
+    elsif user.role? :customer
+      nil
+    else
+      # guest action
+      can :read, Item
+      can :home, Home
+      can :create, User
+      can :create, Customer
+    end
+  end
+
+end
