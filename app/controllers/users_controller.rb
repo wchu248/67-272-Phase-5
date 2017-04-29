7bf37eb8b4a0902dfa5de39d0df0cebb89bdb3fd
Winston Chu <wpchu@andrew.cmu.edu>
2017-04-29 16:16:13 -0400

    didnt fix it before, fixed user thing for real now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      if current_user.role?(:customer)
+      if !logged_in?
+        session[:user_id] = @user.id
         redirect_to home_path, notice: "Welcome, #{@user.first_name}. Thank you for signing up!"
       else
         redirect_to :back, notice: "Successfully created #{@user.proper_name}."
