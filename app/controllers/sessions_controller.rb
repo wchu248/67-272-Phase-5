fdadcaa2310237870b4421ceee4ecb2f584eefa8
Winston Chu <wpchu@andrew.cmu.edu>
2017-05-01 14:27:27 -0400

    create and destroy carts in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < ApplicationController
 
   include ChessStoreHelpers::Cart
-
-  def new
-    create_cart
-  end
   
   def create
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      if user.role?(:admin) || user.role?(:customer)
+        create_cart
+      end
       redirect_to root_url, notice: "Welcome, #{user.first_name}!"
     else
       flash[:error] = "Username or password is invalid."
@@ -22,4 +21,5 @@ class SessionsController < ApplicationController
     destroy_cart
     redirect_to root_url, notice: "Logged out."
   end
+  
 end
