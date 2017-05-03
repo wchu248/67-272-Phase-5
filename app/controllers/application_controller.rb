2fbb673e475a8625094b347d5d4cf4ed33172596
Winston Chu <wpchu@andrew.cmu.edu>
2017-05-03 12:36:00 -0400

    got preselected school to be most recent one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = "You are not authorized to take this action.  Go away or I shall taunt you a second time."
+    flash[:error] = "You are not authorized to take this action."
     redirect_to home_path
   end
 
